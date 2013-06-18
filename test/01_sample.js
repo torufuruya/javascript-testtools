e528660698da4d3a89b2e24ba302236c6e3e7ec4
Toru Furuya <toru.furuya@gree.net>
2013-06-18 19:50:23 +0900

    20:00

--- test/01_sample.js
+++ test/01_sample.js
@@ -18,5 +18,8 @@ describe('nativeCall', function () {
         var spy = sinon.spy(window, 'nativeCall');
         window_stub.args[0][1]();
         expect(spy.called).to.eql(true);
+        expect(spy.args[0][0]).to.be.a('string');
+        expect(spy.args[0][1]).to.be.a('object');
+        expect(spy.args[0][2]).to.be.a('string');
     });
 });
